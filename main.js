29de9cd7cb21f81379d7ab207cd1a0498671cd46
viktorrn <90602664+viktorrn@users.noreply.github.com>
2022-04-27 21:12:20 +0200

    fixed more things with the grapplinghook, and multiple diffirent new mechanics

--- main.js
+++ main.js
@@ -85,8 +85,11 @@ let player = {
     keyCoolDowns :[],
 
     updatesSinceWallSlide:0,
+    updatesSinceGrounded:0,
     lastWallSlideSide:0,
     lastWallJumpSide:0,
+    jumpsLeft:0,
+    
 
     color: "green",
     width: 30,
@@ -96,7 +99,7 @@ let player = {
 let grapplingHook = {
     width:30,
     height:10,
-    drawLine:false,
+    drawLine:true,
     hook:{
         active: false,
         x:0,
@@ -106,15 +109,24 @@ let grapplingHook = {
         velX:0,
         velY:0,
         
-        hookFlySpeed:24,
-        pullAccForce:1.1,
+        flySpeed:24,
+        releaseRadius:50,
+
+        pullStiffness:2,
+        pullAccForce:2,
+        pullHookMaxForce:9,
         pullVx:0,
         pullVy:0,
         pullAng:0,
+        updateCoolDown:0,
 
+    
         connected:false,
         updatesFlying:0,
-        updatesPulling:0,
+        updatesPulling:0,   
+        updatesTillPull:8,
+
+        overHeadSling:0,
         
     }
 
@@ -169,7 +181,21 @@ function projection(vx,vy,x,y){
 
 // logic
 
+function resetHook(hook){
+    hook.connected = false;
+    hook.active = false;
+    hook.velX = 0;
+    hook.velY = 0;
+    hook.overHeadSling = 0;
+    hook.updateCoolDown = 30;
+    player.pullVelX = 0;
+    player.pullVelY = 0;
+
+    return hook;
+}
+
 function calcGrapplingHook(hook){
+    if(hook.updateCoolDown > 0) hook.updateCoolDown--;
     if(!hook.active) return hook;
     
     if(hook.updatesFlying > 20){
@@ -191,7 +217,7 @@ function calcGrapplingHook(hook){
             }
             hook.connected = true;
             hook.updatesPulling = 0;
-            hook.updatesFlying = 0;
+          
         }
         if(rectCollisionCheck(objects, hook.x ,hook.y + hook.velY, hook.width, hook.height)){
             let dir =  Math.sign(hook.velY)
@@ -202,90 +228,97 @@ function calcGrapplingHook(hook){
             }
             hook.connected = true;
             hook.updatesPulling = 0;
-            hook.updatesFlying = 0;
+            
         }
         hook.x += hook.velX;
         hook.y += hook.velY;
     }else{
         hook.updatesPulling++;
+        if(hook.updatesPulling < hook.updatesTillPull)
+        {
+            if(keyStates[' '] == 1)
+            {
+                console.log("overhead")
+                keyStates[' '] == -1;
+                hook.overHeadSling = 1;
+            }
+        } 
         let dist = distance(player.x+player.width/2,player.y+player.height/2,hook.x+hook.width/2 ,  hook.y+hook.height/2)
-        
-       // console.log(dist)
 
-        if(dist < 70){
+        if(dist < hook.releaseRadius){
             hook = resetHook(hook);
              
         }else{
             
-            hook.pullVx = (hook.x+hook.width/2 - player.x+player.width/2);
-            hook.pullVy = (hook.y+hook.height/2  - player.y+player.height/2); 
+            hook.pullVx = (hook.x - player.x);
+            hook.pullVy = (hook.y  - player.y); 
             hook.pullAng = Math.atan2(hook.pullVy , hook.pullVx); 
-            let dsvx = Math.cos(hook.pullAng)*1;
-            let dsvy = Math.sin(hook.pullAng)*1;
-
-            player.pullVelX = dsvx;
-            player.pullVelY = dsvy;
-         
-
-            if(vecLength(player.pullVelX,player.pullVelY) < 6 ){
-         
-    
-                player.velX += dsvx//player.pullVelX;
-                player.velY += dsvy//player.pullVelY;
-
-                
-            }
+            let dsvx = Math.cos(hook.pullAng)*hook.pullHookMaxForce;
+            let dsvy = Math.sin(hook.pullAng)*hook.pullHookMaxForce;
             
+            let diffx = player.velX - dsvx;
+            let diffy = player.velY - dsvy;
+            let diffLen = vecLength(diffx, diffy)
+            let inpDrift = keyStates['d'] - keyStates['a']
+
+            player.velX += dsvx/hook.pullHookMaxForce*hook.pullAccForce -diffx/(diffLen*hook.pullStiffness) +inpDrift*1.3/hook.updatesPulling;
+            player.velY += dsvy/hook.pullHookMaxForce*hook.pullAccForce -diffy/(diffLen*hook.pullStiffness) - hook.overHeadSling*(4/hook.updatesPulling);
         }
         
     }
     return hook;
 }
 
-function resetHook(hook){
-    hook.connected = false;
-    hook.active = false;
-    hook.velX = 0;
-    hook.velY = 0;
-    player.pullVelX = 0;
-    player.pullVelY = 0; 
-    return hook;
-}
 
-function calcPlayer(p){
+
+function calcPlayer(p,keyStates){
     let dshp = ( keyStates['d'] - keyStates['a'])*p.maxSpeed;
-    let airMult = 1;
-    if(p.updatesSinceWallSlide < 11) p.updatesSinceWallSlide++;
     p.grounded = rectCollisionCheck(objects, p.x,p.y+1, p.width, p.height) ? true : false;
+    let airMult = 1;
+    
     if(grapplingHook.hook.connected){
-        
-        if(keyStates[' '] == 1){
-            keyStates[' '] = -1;
-            //grapplingHook.hook =  resetHook(grapplingHook.hook)
-        }
-        if(keyStates['mkl'] == 1){
+        if(keyStates['mkl'] == 1)
+        {
             keyStates['mkl'] = -1;
             grapplingHook.hook =  resetHook(grapplingHook.hook)
         }
     }
     
-    if(p.grounded){
-        p.lastWallSlideSide = 0; p.lastWallJumpSide = 0;
-        if(keyStates[' '] == 1){
-            keyStates[' '] = -1
-            p.velY = -p.jumpPower;
-        }
-    }else{
+
+
+   
+    if(!p.grounded)
+    {
         airMult = 0.5;
-        
-        if(p.velY < p.maxFallSpeed && !grapplingHook.hook.connected) p.velY += p.fallAcc;
-        if(rectCollisionCheck(objects, p.x + Math.sign(dshp)*5,p.y, p.width, p.height) &&  Math.sign(dshp) != p.lastWallJumpSide){
+
+        if(p.updatesSinceGrounded < 10) p.updatesSinceGrounded++;
+        else if( p.updatesSinceGrounded == 10) {
+            p.updatesSinceGrounded == 11;
+            if(p.jumpsLeft > 0)
+                p.jumpsLeft = 1
+        } 
+        if(p.updatesSinceWallSlide < 10) p.updatesSinceWallSlide++; 
+       
+
+        if(p.velY < p.maxFallSpeed && !grapplingHook.hook.connected)  // falling
+        {
+            p.velY += p.fallAcc; 
+        } 
+
+        if(rectCollisionCheck(objects, p.x + Math.sign(dshp)*5,p.y, p.width, p.height) &&  Math.sign(dshp) != p.lastWallJumpSide)
+        {
             p.updatesSinceWallSlide = 0;
             p.lastWallSlideSide = Math.sign(dshp);
-            if( p.velY > 3){ p.velY -= p.fallAcc*2;}
+            if( p.velY > 3){ 
+                p.velY -= p.fallAcc*2;
+            }
             
         }
-        if(keyStates[' '] == 1 && p.updatesSinceWallSlide < 10){
+        
+        if(keyStates[' '] == 1 && p.updatesSinceWallSlide < 10)
+        {
+            p.jumpsLeft = 1;
+            p.updatesSinceWallSlide = 10
             keyStates[' '] = -1;
             p.lastWallJumpSide = p.lastWallSlideSide;
             p.velX -= p.lastWallSlideSide*p.accX*5;
@@ -293,12 +326,41 @@ function calcPlayer(p){
         }
         
     }
+    else
+    {
+        p.jumpsLeft = 2;
+        p.lastWallSlideSide = 0; 
+        p.lastWallJumpSide = 0; 
+        p.updatesSinceGrounded = 0;    
+    }
+    //console.log("updates",p.updatesSinceGrounded)
+
+    if(keyStates[' '] == 1)// jump
+    {  
+        keyStates[' '] = -1
+        if(p.jumpsLeft == 1)
+        {
+            p.updatesSinceGrounded = 11;
+            p.velY = -p.jumpPower*0.8
+            p.jumpsLeft--; 
+        }
+        if(p.jumpsLeft == 2 || p.updatesSinceGrounded < 11)
+        {
+            p.updatesSinceGrounded = 11;
+            p.velY = -p.jumpPower
+            p.jumpsLeft--; 
+        }
+        
+
+        
+    }
 
     if(p.velX > dshp) p.velX -= p.accX*airMult;
     else if(p.velX < dshp) p.velX += p.accX*airMult;
     if(dshp == 0 && Math.abs(p.velX) < 0.5 ) p.velX = 0;
 
-    if(keyStates['mkl'] == 1 && !grapplingHook.hook.active){
+    if(keyStates['mkl'] == 1 && !grapplingHook.hook.active && grapplingHook.hook.updateCoolDown == 0)
+    {
         keyStates['mkl'] = -1;
         let vx = ( mousePosition.x - p.x-p.width/2) ;
         let vy = (mousePosition.y - p.y-p.height/2) ;  
@@ -308,44 +370,52 @@ function calcPlayer(p){
         grapplingHook.hook.updatesFlying = 0;
         grapplingHook.hook.x = p.x+p.width/2;
         grapplingHook.hook.y = p.y+p.height/2;
-        grapplingHook.hook.velX = Math.cos(a)*grapplingHook.hook.hookFlySpeed;
-        grapplingHook.hook.velY = Math.sin(a)*grapplingHook.hook.hookFlySpeed;
+        grapplingHook.hook.velX = Math.cos(a)*grapplingHook.hook.flySpeed;
+        grapplingHook.hook.velY = Math.sin(a)*grapplingHook.hook.flySpeed;
    
         
     }
 
     // collisions
-    if(rectCollisionCheck(objects, p.x + p.velX,p.y, p.width, p.height)){
+   
+
+    if(rectCollisionCheck(objects, p.x + p.velX,p.y, p.width, p.height))
+    {
         let dir =  Math.sign(p.velX)
         
-        while(!rectCollisionCheck(objects, p.x + dir, p.y, p.width, p.height)){   
+        while(!rectCollisionCheck(objects, p.x + dir, p.y, p.width, p.height))
+        {   
             p.x += dir;
         }
         p.velX = 0;
     }
 
     
-    if(rectCollisionCheck(objects, p.x ,p.y + p.velY, p.width, p.height)){
+    if(rectCollisionCheck(objects, p.x ,p.y + p.velY, p.width, p.height))
+    {
         let dir =  Math.sign(p.velY)
         
-        while(!rectCollisionCheck(objects, p.x , p.y + dir, p.width, p.height)){   
+        while(!rectCollisionCheck(objects, p.x , p.y + dir, p.width, p.height))
+        {   
             p.y += dir;
         }
         p.velY = 0;
     }
 
-    if(rectCollisionCheck(objects, p.x + p.velX ,p.y + p.velY, p.width, p.height)){
+    if(rectCollisionCheck(objects, p.x + p.velX ,p.y + p.velY, p.width, p.height))
+    {
         let dirY =  Math.sign(p.velY)
         let dirX =  Math.sign(p.velX)
         p.velY = 0;
         
-        while(!rectCollisionCheck(objects, p.x+ dirX , p.y + dirY, p.width, p.height)){   
+        while(!rectCollisionCheck(objects, p.x+ dirX , p.y + dirY, p.width, p.height))
+        {   
             p.y += dirY;
             p.x += dirX;
         }
         if(rectCollisionCheck(objects, p.x+ dirX , p.y, p.width, p.height))
             p.velX = 0;
-            else
+        else
             p.velY = 0;
         
     }
@@ -360,7 +430,7 @@ function calcPlayer(p){
 function drawPlayer(player){
     ctx.beginPath();
     ctx.translate(player.x + player.width/2, player.y + player.height/2 );
-    ctx.rotate(player.velX/(10*player.maxSpeed));
+    ctx.rotate( (player.velX^2) /(10*player.maxSpeed));
     
     ctx.rect(-player.width/2,-player.height/2,player.width * 1 ,player.height * (1+ Math.abs(player.velY/50 )) );
     ctx.fillStyle = player.color;
@@ -403,7 +473,7 @@ function draw(){
 
 function update(){
     grapplingHook.hook = calcGrapplingHook(grapplingHook.hook);
-    player = calcPlayer(player);
+    player = calcPlayer(player,keyStates);
     if(keyStates['l'] == 0){
         keyStates['l'] = -1;
         console.log(player,grapplingHook.hook);
